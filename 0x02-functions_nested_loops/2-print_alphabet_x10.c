cb7070086a1be040a7a55b717270f716e9d519ed
shadenelnaggar <shadensherif987@gmail.com>
2023-07-18 14:18:16 -0700

    committing

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,10 +13,10 @@ void print_alphabet_x10(void)
 {
 	int l, ch;
 
-	for (l = 0; l <= 0; l++)
+	for (l = 0; l <= 9; l++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
-		_putchar('\n');
+		_putchar('\n')i;
 	}
 }
