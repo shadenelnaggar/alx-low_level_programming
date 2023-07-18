0d5780eadc7fa92d89d248910c8772e3aa9d24b2
shadenelnaggar <shadensherif987@gmail.com>
2023-07-18 13:27:04 -0700

    committing

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: printing _putchar using putchar prototype
+ *
+ * Return: 0 (Succes)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
