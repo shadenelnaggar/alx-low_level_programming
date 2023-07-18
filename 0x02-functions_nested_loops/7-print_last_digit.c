4988a002f3a9b120ca0f1d72a606b8597cc7e0d0
shadenelnaggar <shadensherif987@gmail.com>
2023-07-18 15:15:29 -0700

    committing

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,10 +13,9 @@ int print_last_digit(int n)
 {
 	int ld;
 
-	if (n < 0)
-		ld = -1 * (n % 10);
-	else
-		ld = n % 10;
+	ld = n % 10;
+	if (ld < 0)
+		ld = ld + -1;
 
 	_puthcar(ld + '0');
 	return (ld);
