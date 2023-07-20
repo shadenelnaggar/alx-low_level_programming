8b8092b42ca3125001ca80baa8ab2a02ac740338
shadenelnaggar <shadensherif987@gmail.com>
2023-07-20 16:45:21 -0700

    committing

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - printing a diagonal line
+ * @n: input of type integer
+*/
+
+void print_diagonal(int n)
+{
+	int p, sp;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (p = 1; p <= n; p++)
+		{
+			for (sp = 1; sp <= p; sp++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
