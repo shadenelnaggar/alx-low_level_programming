c077a0b9402ea41179a0c99f2b7439fc36757671
shadenelnaggar <shadensherif987@gmail.com>
2023-07-20 13:50:38 -0700

    committing

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints digits from 0 - 9 using _putchar function
+ * Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(num + 48);
+		num++;
+	}
+	_putchar('\n');
+}
