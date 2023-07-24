9469b9add1a4b67787c6121d8c466335766d73db
shadenelnaggar <shadensherif987@gmail.com>
2023-07-24 13:47:53 -0700

    committing

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,11 +14,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = -1;
 
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
+	do {
 		i++;
-	}
+		dest[i] = src[i];
+	} while (src[i] != '\0')
 
 	return (dest);
 }
