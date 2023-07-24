458b567c20ff68fdae94f744977fa3583a96f939
shadenelnaggar <shadensherif987@gmail.com>
2023-07-24 14:55:54 -0700

    committing

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,16 +11,16 @@
 int _atoi(char *s)
 {
 	unsigned int n = 0;
-	int s = 1;
+	int si = 1;
 
 	do {
 		if (*s == '-')
-			s *= -1;
+			si *= -1;
 		else if (*s >= '0' && *s <= '9')
 			n = (n * 10) + (*s - '0');
 		else if (n > 0)
 			break;
 	} while (*s++);
 
-	return (n * s);
+	return (n * si);
 }
