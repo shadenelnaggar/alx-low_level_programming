71a1b4d29858353edc69ad892959bc462b4abe72
shadenelnaggar <shadensherif987@gmail.com>
2023-07-28 16:00:22 -0700

    committing

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - a function that encodes a string into 1337
+ * @s: string input
+ * Return: encoded string into 1337
+*/
+
+char *leet(char *s)
+{
+	char *sp = s;
+	char k[] = {'A', 'E', 'O', 'T', 'L'};
+	int val[] = {4, 3, 0, 7, 1};
+	unsigned int i;
+
+	while (*s)
+	{
+		for (i = 0; i < sizeof(k) / sizeof(char); i++)
+		{
+			if (*s == k[i] || *s == k[i] + 32)
+			{
+				*s = 48 + val[i];
+			}
+		}
+		s++
+	}
+
+	return (*sp);
+}
