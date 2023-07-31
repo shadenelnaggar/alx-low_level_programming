22253ea279896018e70abe0279e6f26736d97af7
shadenelnaggar <shadensherif987@gmail.com>
2023-07-31 15:54:15 -0700

    committing

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,6 +2,27 @@
 
 /**
  * *_strstr - a function that locates a substring
- * @haystack: string
- * @needle:
- * Return:*/
+ * @haystack: pointer to string
+ * @needle: pointer to substring
+ * Return: 0
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *s = haystack;
+		char *p = needle;
+
+		while (*s == *p && *p != '\0')
+		{
+			s++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
