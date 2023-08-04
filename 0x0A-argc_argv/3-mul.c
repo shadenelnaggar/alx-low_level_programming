abe76df23405b7fc94a80f4dc192cb24553a00de
shadenelnaggar <shadensherif987@gmail.com>
2023-08-04 15:38:06 -0700

    committing

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,18 +10,17 @@
 
 int main(int argc, char *argv[])
 {
-	int tot;
+	int x, y;
 
-	if (argc == 3)
-	{
-		tot = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", tot);
-	}
-	else
+	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	printf("%d\n", x + y);
 
 	return (0);
 }
