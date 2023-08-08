5cbab083967dee60720a607d0e63a0cc2d36a7de
shadenelnaggar <shadensherif987@gmail.com>
2023-08-08 16:02:20 -0700

    committing

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * strlen - determines the length of the string
+ * _strlen - determines the length of the string
  * @str: string input
  * Return: the length of the string
 */
 
-int strlen(char *str)
+int _strlen(char *str)
 {
 	int s = 0;
 
@@ -32,13 +32,13 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "\0";
 
-	sz1 = strlen(s1);
-	sz2 = strlen(s2);
+	sz1 = _strlen(s1);
+	sz2 = _strlen(s2);
 	n = malloc((sz1 + sz2) * sizeof(char) + 1);
 	if (n == 0)
 		return (0);
 
-	for (i = 0; i <= (sz1 + sz2); i++)
+	for (i = 0; i <= sz1 + sz2; i++)
 	{
 		if (i < sz1)
 			n[i] = s1[i];
