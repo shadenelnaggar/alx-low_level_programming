c62deb467b325668653a0ac3972738e973ca1274
shadenelnaggar <shadensherif987@gmail.com>
2023-08-10 17:20:03 -0700

    committing

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,6 +13,8 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int i;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
@@ -36,10 +38,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (result == NULL)
 		return (NULL);
 
-	for (unsigned int i = 0; i < l1; i++)
+	for (i = 0; i < l1; i++)
 		result[i] = s1[i];
 
-	for (unsigned int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		result[l1 + i] = s2[i];
 
 	result[tot - 1] = '\0';
