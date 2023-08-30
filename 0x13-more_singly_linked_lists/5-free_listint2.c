523670a8e5e2f22af29849f4cd6a230b0a4db8a9
shadenelnaggar <shadensherif987@gmail.com>
2023-08-29 17:28:04 -0700

    committing

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,13 +2,13 @@
 
 /**
  * free_listint2 - a function that frees a listint_t list.
- * @head: first pointer
+ * @head: address of first pointer
  * Return: none
 */
 
 void free_listint2(listint_t **head)
 {
-	listint_t *nd, tmp;
+	listint_t *nd, *tmp;
 
 	if (!head)
 		return;
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - a function that inserts a new node at a
+ * given position.
+ * @head: first node pointer
+ * @idx: index to enter new node
+ * Return: new node addr
+*/
